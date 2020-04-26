988dc58a72de6775c77732f0958d3f3dbd1ff137
RYUHYEREE <111gpfl@naver.com>
2020-04-26 16:53:04 +0900

    v1.0

---
+++ homework5/src/com/hyeree/OSHW/gugudan.java
@@ -0,0 +1,17 @@
+package com.hyeree.OSHW;
+import java.util.Scanner;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		
+		System.out.print("input the number : ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1;i<=9;i++) {
+			System.out.println(n+"*"+i+"="+(n*i));
+		}
+
+	}
+
+}
